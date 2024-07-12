f8448f3b4f096828fd52b02e0942d8fc078cb4a6
silentFellow <thegreatestintelligent+github@gmail.com>
2024-07-12 12:27:35 +0530

    env variable added and publically available

--- server/src/data/dataCollection.py
+++ server/src/data/dataCollection.py
@@ -3,6 +3,10 @@ import requests
 from time import sleep
 import polars as pl
 
+# for env
+from dotenv import load_dotenv
+import os
+
 """ years = np.arange(2015, 2024)
 months = np.arange(1, 13)
 endDates = np.array([31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]) """
@@ -15,33 +19,33 @@ try:
   df = pl.read_csv("rawData.csv")
 except:
   df = pl.DataFrame({
-    'cityName': [], 
-    'temp': [], 
-    'feelsLike': [], 
-    'pressure': [], 
-    'humidity': [], 
-    'clouds': [], 
-    'windSpeed': [], 
-    'windDir': [], 
-    'gustSpeed': [], 
+    'cityName': [],
+    'temp': [],
+    'feelsLike': [],
+    'pressure': [],
+    'humidity': [],
+    'clouds': [],
+    'windSpeed': [],
+    'windDir': [],
+    'gustSpeed': [],
     'result': []
   }, schema = {
-    'cityName': str, 
-    'temp': float, 
-    'feelsLike': float, 
-    'pressure': float, 
-    'humidity': float, 
-    'clouds': float, 
-    'windSpeed': float, 
-    'windDir': float, 
-    'gustSpeed': float, 
+    'cityName': str,
+    'temp': float,
+    'feelsLike': float,
+    'pressure': float,
+    'humidity': float,
+    'clouds': float,
+    'windSpeed': float,
+    'windDir': float,
+    'gustSpeed': float,
     'result': str
   })
 
-GEO_ENCODING_API_KEY = "69a1dcf5b06f61a63bbf2d30ee3651a1"
-""" WEATHER_BIT_API_KEY = "30ce3b09c6a54e5abcc7fc107d23f767" """
-WEATHER_BIT_API_KEY = "e54b6b8fb2b348f88e942f98059d0c27"
-
+load_dotenv()
+GEO_ENCODING_API_KEY = os.getenv("GEO_ENCODING_API_KEY")
+""" WEATHER_BIT_API_KEY = os.getenv("WEATHER_BIT_API_KEY_1") """
+WEATHER_BIT_API_KEY = os.getenv("WEATHER_BIT_API_KEY_2")
 
 """ cities = np.array(['Erode', 'Coimbatore', 'Chennai', 'Tiruppur', 'Karur', 'Namakkal', 'Nilgiris', 'Thanjavur', 'Vellore', 'Dharmapuri']) """
 cities = np.array(['Erode', 'Coimbatore'])
@@ -52,9 +56,9 @@ for city in cities:
   res = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=5&appid={GEO_ENCODING_API_KEY}")
   if res.status_code != 200:
     continue
-  
+
   res = res.json()
-  
+
   filteredRes = np.array(list(filter(lambda fromTN: fromTN['name'].lower() == city.lower() and fromTN['state'].lower() == 'tamil nadu', res)))
   if len(filteredRes) == 0:
     continue
@@ -72,15 +76,15 @@ for city in cities:
       response = response.json()
       for res in response['data']:
         info = pl.DataFrame({
-        'cityName': city, 
-        'temp': float(res['temp']), 
-        'feelsLike': float(res['app_temp']), 
-        'pressure': float(res['pres']), 
-        'humidity': float(res['rh']), 
-        'clouds': float(res['clouds']), 
-        'windSpeed': float(res['wind_spd']), 
-        'windDir': float(res['wind_dir']), 
-        'gustSpeed': float(res['wind_gust_spd']), 
+        'cityName': city,
+        'temp': float(res['temp']),
+        'feelsLike': float(res['app_temp']),
+        'pressure': float(res['pres']),
+        'humidity': float(res['rh']),
+        'clouds': float(res['clouds']),
+        'windSpeed': float(res['wind_spd']),
+        'windDir': float(res['wind_dir']),
+        'gustSpeed': float(res['wind_gust_spd']),
         'result': res['weather']['description']
       })
 
